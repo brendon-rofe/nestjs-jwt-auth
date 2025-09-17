4d5131891db3ef9daa1067d97b1115a5bdde2dfd
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-09-17 15:34:44 +0200

    feat: Create inital login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthPayloadDto } from './dto/auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+  @Post('login')
+  login(@Body() authPayload: AuthPayloadDto ) {
+    
+  }
+
+}
