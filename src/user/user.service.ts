1e5318c2659d6bb05e4fe5b0b2deb12741897165
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-09-19 16:03:16 +0200

    feat: Create create method for UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(dto: CreateUserDto) {
+    return await this.prisma.user.create({
+      data: dto,
+    });
+  }
+
+}
