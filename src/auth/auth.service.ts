c263a63091e9b6ad9c889f0cc3233ab1fb60770e
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-09-20 18:10:11 +0200

    fix: remove hard coded users array

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { AuthPayloadDto } from './dto/auth.dto';
 
-const users = [
-  {
-    id: 1,
-    username: 'john',
-    password: 'password'
-  },
-  {
-    id: 2,
-    username: 'jane',
-    password: 'pass123'
-  }
-]
-
 @Injectable()
 export class AuthService {
 
   validateUser(authPayloadDto: AuthPayloadDto) {
-
+    
   }
 
 }
