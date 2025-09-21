5ab3ad483ddea995b03b8e2f53d7a821d7c32ca4
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-09-21 13:33:29 +0200

    feat: Create initial hash password method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,10 @@ import { AuthPayloadDto } from './dto/auth.dto';
 @Injectable()
 export class AuthService {
 
+  async hashPassword(password: string) {
+    
+  }
+
   validateUser(authPayloadDto: AuthPayloadDto) {
     
   }
