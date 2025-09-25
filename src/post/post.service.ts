fae1a6d3efb787b5d66f9cbe35368cf8e177ffac
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-09-25 15:31:54 +0200

    feat: Create Post CRUD methods

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreatePostDto } from './dto/creat-post.dto';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(private prisma: PrismaService) {}
+
+  async createPost(userid: number, createPostData: CreatePostDto) {
+    return await this.prisma.post.create({
+      data: { authorId: userid, ...createPostData},
+    });
+  }
+
+  async getUserPosts(userid: number) {
+    return await this.prisma.post.findMany({
+      where: { authorId: userid }
+    });
+  }
+
+  async getPostById(id: number) {
+    return await this.prisma.post.findUnique({
+      where: { id }
+    });
+  }
+
+  async updatePost(id: number, updatePostData: CreatePostDto) {
+    return await this.prisma.post.update({
+      where: { id },
+      data: { ...updatePostData }
+    });
+  }
+
+  async deletePost(id: number) {
+    return await this.prisma.post.delete({
+      where: { id }
+    });
+  }
+
+}
