a79403d9d27081bcbed1ad4101349ed4e1abfeea
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-09-25 14:02:22 +0200

    fix: Remove secret and signOptions from JwtModule.register

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,10 +6,7 @@ import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
-    }),
+    JwtModule.register({}),
     UserModule
   ],
   controllers: [AuthController],
