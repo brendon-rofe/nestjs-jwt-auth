ffce822a38dddf296ef44754abf0bbfa18533311
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-09-25 14:03:48 +0200

    fix: Revert

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,7 +6,10 @@ import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    JwtModule.register({}),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
+    }),
     UserModule
   ],
   controllers: [AuthController],
