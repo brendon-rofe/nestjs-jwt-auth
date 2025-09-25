cac970e2931a5c357a9e002406dd147fe3814f9b
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-09-25 11:05:04 +0200

    feat: Import JWT module into Auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
+    }),
+    UserModule
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
